1ad581716177512626a422bc15d2422c2ceac037
BlackCoderZed <41987824+BlackCoderZed@users.noreply.github.com>
2021-08-17 16:54:54 +0630

    Create itsolution-installer.sh

---
+++ itsolution-installer.sh
@@ -0,0 +1,8 @@
+sudo su
+curl https://packages.microsoft.com/config/ubuntu/21.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+exit
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
